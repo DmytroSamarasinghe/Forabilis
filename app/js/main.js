375ee8d7481f39e900e7f521f20809513a954185
DmytroSmarasinghe <dima35x@ukr.net>
2020-07-30 17:43:55 +0300

    added adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,39 +1,40 @@
 $(function () {
 
-    $(".clients__slider-wrapper").slick({
+    $(".clients__slider-inner").slick({
         dots: false,
-        slidesToShow: 1,
+        infinite: false,
+        slidesToShow: 2,
         slidesToScroll: 1,
         appendArrows: $('.clients__slider-button'),
         nextArrow: '<button type="submit" class="clients__slider-button-right button-box button-slider"></button>',
         prevArrow: '<button type="submit" class="clients__slider-button-left button-box button-slider"></button>',
-        // responsive: [
-        //     {
-        //         breakpoint: 1900,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             slidesToScroll: 3,
-        //             infinite: true,
-        //             dots: true
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 1441,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 1,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 801,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1,
-        //         }
-        //     },
-        // ]
+        responsive: [
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 860,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    variableWidth: false
+                }
+            },
+            //     {
+            //         breakpoint: 801,
+            //         settings: {
+            //             slidesToShow: 1,
+            //             slidesToScroll: 1,
+            //         }
+            //     },
+        ]
     });
-}); 
+});
 $('.menu__btn').on('click', function () {
     $('.menu__list').slideToggle();
 });
